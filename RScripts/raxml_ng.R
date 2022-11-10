0e9603d5689b7dd437e70755e22447ab56582e33
rupert@sorubim <rupertcollins@gmail.com>
2022-11-10 22:25:07 +0000

    add new raxml function

--- RScripts/raxml_ng.R
+++ RScripts/raxml_ng.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-# NEW RAXML-NG FUN
+# OLD RAXML-NG FUN
 raxml_ng <- function(file,verbose) {
     if(verbose == "true") {
         string.mafft <- paste0("mafft --thread -1 --maxiterate 2 --retree 2 ",file," > ",file,".ali")
@@ -19,5 +19,27 @@ raxml_ng <- function(file,verbose) {
         system(command=string.search,ignore.stdout=TRUE)
         rax.tr <- ape::read.tree(file=paste0(file,".ali.raxml.rba.raxml.bestTree"))
     } else stop(writeLines("'-v' value must be 'true' or 'false'."))
+    return(rax.tr)
+}
+
+# NEW RAXML-NG FUN
+raxml_align <- function(file,align,model,epsilon) {# TN93+F+G
+    if (!is.logical(align)) 
+        stop(writeLines("Value must be TRUE or FALSE."))
+    if(align) {
+        string.mafft <- paste0("mafft --thread -1 --auto ",file," > ",file,".ali")
+        system(command=string.mafft,ignore.stdout=FALSE)
+        string.parse <- paste0("raxml-ng --parse --msa ",file,".ali --model ",model," --seed 42 --redo --threads auto")
+        system(command=string.parse,ignore.stdout=FALSE)
+        string.search <- paste0("raxml-ng --search --msa ",file,".ali.raxml.rba --tree pars{1} --lh-epsilon ",epsilon," --seed 42 --redo --threads auto")
+        system(command=string.search,ignore.stdout=FALSE)
+        rax.tr <- ape::read.tree(file=paste0(file,".ali.raxml.rba.raxml.bestTree"))
+    } else {
+        string.parse <- paste0("raxml-ng --parse --msa ",file," --model ",model," --seed 42 --redo --threads auto")
+        system(command=string.parse,ignore.stdout=FALSE)
+        string.search <- paste0("raxml-ng --search --msa ",file,".raxml.rba --tree pars{1} --lh-epsilon ",epsilon," --seed 42 --redo --threads auto")
+        system(command=string.search,ignore.stdout=FALSE)
+        rax.tr <- ape::read.tree(file=paste0(file,".raxml.rba.raxml.bestTree"))
+    }
     return(rax.tr)
 }
