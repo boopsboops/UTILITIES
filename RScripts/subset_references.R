363bcbee405968baffc133fc69adb9daca0689aa
rupert@jau <rupertcollins@gmail.com>
2022-12-03 18:53:32 +0000

    added references subset fun

---
+++ RScripts/subset_references.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+# script to subset desired references
+
+# load up cleaning fun
+subset_references <- function(df,frag) {
+    frag <- paste0("nucleotidesFrag.",frag,".noprimers")
+    reflib.sub <- df %>% dplyr::filter(!is.na(!!as.name(frag))) %>%
+        dplyr::mutate(nucleotides=!!as.name(frag), length=!!as.name(str_replace_all(frag,"nucleotides","length"))) %>%
+        select(-contains("Frag"))
+    return(reflib.sub)
+}
