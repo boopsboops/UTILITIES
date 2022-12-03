0840598f25e3ab6cb0fae9548e1370a7aaf9e505
rupert@jau <rupertcollins@gmail.com>
2022-12-03 22:37:32 +0000

    revert

--- RScripts/subset_references.R
+++ RScripts/subset_references.R
@@ -3,7 +3,6 @@
 
 # load up cleaning fun
 subset_references <- function(df,frag) {
-    writeLines("testing")
     frag <- paste0("nucleotidesFrag.",frag,".noprimers")
     reflib.sub <- df %>% dplyr::filter(!is.na(!!as.name(frag))) %>%
         dplyr::mutate(nucleotides=!!as.name(frag), length=!!as.name(str_replace_all(frag,"nucleotides","length"))) %>%
